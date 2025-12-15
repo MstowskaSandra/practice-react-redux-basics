7db6b7c5b508f57e50af269be2b8e6b509e156f7
Sandra <sandra.mstowskaa@gmail.com>
2025-12-15 16:19:31 +0100

    task02: feat: create redux store

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 // ./src/app.js
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-import App from './components/App';
+import App from "./components/App";
+const initState = {
+  message: "Działa!",
+};
 
-const root = createRoot(document.querySelector('#root'));
-root.render(<App />);
+const reducer = (state = initState, action) => {
+  return state;
+};
+
+const store = createStore(reducer);
+
+const root = createRoot(document.querySelector("#root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
